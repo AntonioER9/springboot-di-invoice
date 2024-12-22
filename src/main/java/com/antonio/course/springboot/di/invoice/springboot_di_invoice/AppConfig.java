8992fc9644a1bf43e15c61a2565e6c395a6c3554
AntonioER9 <aespinozareyes9@gmail.com>
2024-12-22 09:29:33 -0300

    Add item and product classes with constructors; update AppConfig for invoice items

--- src/main/java/com/antonio/course/springboot/di/invoice/springboot_di_invoice/AppConfig.java
+++ src/main/java/com/antonio/course/springboot/di/invoice/springboot_di_invoice/AppConfig.java
@@ -1,10 +1,25 @@
 package com.antonio.course.springboot.di.invoice.springboot_di_invoice;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+
+import com.antonio.course.springboot.di.invoice.springboot_di_invoice.models.Item;
+import com.antonio.course.springboot.di.invoice.springboot_di_invoice.models.Product;
 
 @Configuration
 @PropertySource("classpath:data.properties")
 public class AppConfig {
+
+  @Bean
+  List<Item> itemsInvoice() {
+    Product product1 = new Product("Camara Sony", 100);
+    Product product2 = new Product("Bicicleta Bianchi aro 26", 200);
+    List<Item> items = Arrays.asList(new Item(product1, 2), new Item(product2, 4));
+    return items;
+  }
 
 }
