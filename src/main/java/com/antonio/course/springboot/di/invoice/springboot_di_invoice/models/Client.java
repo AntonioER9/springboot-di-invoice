52adb188a2f99e6f54ff4410afb50df8c76e7652
AntonioER9 <aespinozareyes9@gmail.com>
2024-12-22 09:24:12 -0300

    Add configuration and properties for Client and Invoice classes

--- src/main/java/com/antonio/course/springboot/di/invoice/springboot_di_invoice/models/Client.java
+++ src/main/java/com/antonio/course/springboot/di/invoice/springboot_di_invoice/models/Client.java
@@ -1,11 +1,15 @@
 package com.antonio.course.springboot.di.invoice.springboot_di_invoice.models;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Client {
 
+  @Value("${client.name}")
   private String name;
+
+  @Value("${client.lastname}")
   private String lastname;
 
   public String getName() {
